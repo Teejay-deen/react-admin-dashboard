ae9ddc39267ea779e5a56625d52adf3b074c1918
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-10-13 10:56:39 +0100

    install some packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-600 underline">Test</h1>
+    <div className="font-inder">
+      <h1 className="text-3xl text-red-600">Test</h1>
     </div>
   );
 }
