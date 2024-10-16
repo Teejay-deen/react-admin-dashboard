b3ff10f236f1d1779d32f89b391ec739efe48fe5
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-10-16 18:48:52 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 function App() {
   return (
     <div className="font-inder">
-      <h1 className="text-3xl text-red-600">Test</h1>
+      <h1 className="text-3xl text-red-600"></h1>
     </div>
   );
 }
