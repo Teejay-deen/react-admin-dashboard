fc7b31a9a8fed9da0e8623a5dae4d7599e4451dc
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-10-18 03:33:27 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
       <h1 className="text-3xl bg-blue-700 mb-2 px-4">Test</h1>
       <section className="grid grid-cols-3 gap-3 px-4">
         <div className="col-span-2 border rounded-lg bg-yellow-500 h-40 p-3"></div>
+        <div></div>
       </section>
     </div>
   );
