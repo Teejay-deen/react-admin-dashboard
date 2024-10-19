328ea26426aadef7b219daa494922ab43971decb
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-10-19 11:47:29 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ function App() {
     <div className="font-inder">
       <h1 className="text-3xl bg-blue-700 mb-2 px-4">Test</h1>
       <section className="grid grid-cols-3 gap-3 px-4">
-        <div className="col-span-2 border rounded-lg h-40 p-3">
-          Good
-        </div>
+        <div className="col-span-2 border rounded-lg h-40 p-3"></div>
         <div></div>
       </section>
     </div>
